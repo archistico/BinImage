06f35988719de56554f77e5f6eb3bc948561b6a4
unknown <info@archistico.com>
2020-03-21 20:26:03 +0100

    'iniziale'

---
+++ binimage.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"github.com/goccy/go-yaml"
+	"image"
+	"image/color"
+	"image/png"
+	"io/ioutil"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	fmt.Println("---BINIMAGE---")
+    const fileConf = "file.yaml"
+
+	// SCRITTURA FILE CONFIGURAZIONE
+
+	var v struct {
+		Lunghezza int `yaml:"Lunghezza"`
+		NomeFile string `yaml:"NomeFile"`
+	}
+
+	/*
+	v.Lunghezza = 100010001
+	v.NomeFile = "nomefile.txt"
+
+	bytes, errMarshal := yaml.Marshal(v)
+	check(errMarshal)
+
+	fmt.Println(string(bytes))
+
+	err := ioutil.WriteFile(fileConf, bytes, 0644)
+	check(err)
+	*/
+
+	// LETTURA FILE CONFIGURAZIONE
+
+	dat, err := ioutil.ReadFile(fileConf)
+	check(err)
+
+	errUnmarshal := yaml.Unmarshal([]byte(dat), &v)
+	check(errUnmarshal)
+
+	fmt.Println(v.NomeFile)
+	dataLength := len(dat)
+
+	// Create image
+
+	width := 200
+	height := 100
+
+	upLeft := image.Point{0, 0}
+	lowRight := image.Point{width, height}
+
+	img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
+
+	indexData := 0
+
+	// Set color for each pixel.
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+
+			var Red byte = 0
+			var Green byte = 0
+			var Blue byte = 0
+			var Alpha byte = 0
+
+			if indexData < dataLength {
+				Red = dat[indexData]
+			}
+
+			indexData++
+
+			if indexData < dataLength {
+				Green = dat[indexData]
+			}
+
+			indexData++
+
+			if indexData < dataLength {
+				Blue = dat[indexData]
+			}
+
+			indexData++
+
+			if indexData < dataLength {
+				Alpha = dat[indexData]
+			}
+
+			indexData++
+
+			img.Set(x, y, color.RGBA{Red, Green, Blue, Alpha})
+		}
+	}
+
+	fmt.Println(img.RGBAAt(0,0))
+
+	// Encode as PNG.
+	f, _ := os.Create("image.png")
+	png.Encode(f, img)
+
+}
