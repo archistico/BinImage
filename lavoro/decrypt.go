42ed6887b1a483cca7fb87f343f8188c4d22716a
unknown <info@archistico.com>
2020-04-07 13:16:15 +0200

    ok read png pixel

--- lavoro/decrypt.go
+++ lavoro/decrypt.go
@@ -6,6 +6,7 @@ import (
 	"github.com/goccy/go-yaml"
 	"image"
 	"image/png"
+	"io"
 	"io/ioutil"
 	"os"
 )
@@ -27,6 +28,42 @@ type FileConf struct {
 	Sha1 string `yaml:"Sha1"`
 }
 
+// Get the bi-dimensional pixel array
+func getPixels(file io.Reader) ([][]Pixel, error) {
+	img, _, err := image.Decode(file)
+
+	if err != nil {
+		return nil, err
+	}
+
+	bounds := img.Bounds()
+	width, height := bounds.Max.X, bounds.Max.Y
+
+	var pixels [][]Pixel
+	for y := 0; y < height; y++ {
+		var row []Pixel
+		for x := 0; x < width; x++ {
+			row = append(row, rgbaToPixel(img.At(x, y).RGBA()))
+		}
+		pixels = append(pixels, row)
+	}
+
+	return pixels, nil
+}
+
+// img.At(x, y).RGBA() returns four uint32 values; we want a Pixel
+func rgbaToPixel(r uint32, g uint32, b uint32, a uint32) Pixel {
+	return Pixel{int(r / 257), int(g / 257), int(b / 257), int(a / 257)}
+}
+
+// Pixel struct example
+type Pixel struct {
+	R int
+	G int
+	B int
+	A int
+}
+
 func main() {
 
 	// VARIABILI
@@ -46,18 +83,34 @@ func main() {
 
 	// VARIABILI
 	// var imgs []image.Image
-	upLeft := image.Point{0, 0}
-	lowRight := image.Point{fileConfYaml.LarghezzaImmagine, fileConfYaml.AltezzaImmagine}
+	image.RegisterFormat("png", "png", png.Decode, png.DecodeConfig)
 
 	// CREAZIONE NOMI IMMAGINI DA LEGGERE
 	for c:=0; c<fileConfYaml.NumeroImmagini ; c++ {
-		nf := fmt.Sprintf("%s-%03d.%s\n", fileConfYaml.NomeImmagine, c, fileConfYaml.EstensioneImmagine)
+		nf := fmt.Sprintf("%s-%03d.%s", fileConfYaml.NomeImmagine, c, fileConfYaml.EstensioneImmagine)
 		fmt.Println(nf)
 
-		f, _ := os.Open(nf)
-		img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
-		png.Encode(f, img)
-		fmt.Println(img.RGBAAt(0,0))
+		file, err := os.Open(nf)
+
+		if err != nil {
+			fmt.Println("Error: File could not be opened")
+			os.Exit(1)
+		}
+
+		defer file.Close()
+
+		pixels, err := getPixels(file)
+
+		if err != nil {
+			fmt.Println("Error: Image could not be decoded")
+			os.Exit(1)
+		}
+
+		fmt.Println(pixels)
+
+
+
+
 	}
 
 
